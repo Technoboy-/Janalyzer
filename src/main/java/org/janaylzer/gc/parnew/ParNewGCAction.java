39d9160c09a02575192ed0a43184524c8f06b0ce
dk.technoboy <guo.jiwei@immomo.com>
2019-03-18 19:33:25 +0800

    init

--- src/main/java/org/janaylzer/gc/parnew/ParNewGCAction.java
+++ src/main/java/org/janaylzer/gc/parnew/ParNewGCAction.java
@@ -2,7 +2,10 @@ package org.janaylzer.gc.parnew;
 
 import org.janaylzer.gc.GCAction;
 import org.janaylzer.gc.GCData;
+import org.janaylzer.gc.GCType;
+import org.janaylzer.util.StringUtils;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.janaylzer.util.Constants.*;
@@ -33,11 +36,55 @@ public class ParNewGCAction implements GCAction {
             HEAP_SIZE +
             ">\\d+\\w)\\),\\s(?<" +
             PARNEW_DURATION +
-            ">\\d+\\.\\d+)\\ssecs\\]\\s*";
+            ">\\d+\\.\\d+)\\ssecs\\]";
 
     private static final Pattern PARNEW_PATTERN = Pattern.compile(PARNEW_ACTION);
 
     public void action(String message, GCData data){
-
+        if(!message.contains(PARNEW)){
+            return;
+        }
+        Matcher matcher = PARNEW_PATTERN.matcher(message);
+        if(!matcher.find()){
+            return;
+        }
+        String caution;
+        if(StringUtils.isNotEmpty(caution = matcher.group(PARNEW_CAUTION))){
+            data.addProperties(PARNEW_CAUTION, caution);
+        }
+        String youngUsageBefore;
+        if(StringUtils.isNotEmpty(youngUsageBefore = matcher.group(PARNEW_YOUNG_USAGE_BEFORE))){
+            data.addProperties(PARNEW_YOUNG_USAGE_BEFORE, youngUsageBefore);
+        }
+        String youngUsageAfter;
+        if(StringUtils.isNotEmpty(youngUsageAfter = matcher.group(PARNEW_YOUNG_USAGE_AFTER))){
+            data.addProperties(PARNEW_YOUNG_USAGE_AFTER, youngUsageAfter);
+        }
+        String youngSize;
+        if(StringUtils.isNotEmpty(youngSize = matcher.group(PARNEW_YOUNG_SIZE))){
+            data.addProperties(PARNEW_YOUNG_SIZE, youngSize);
+        }
+        String cleanupDuration;
+        if(StringUtils.isNotEmpty(cleanupDuration = matcher.group(PARNEW_CLEANUP_DURATION))){
+            data.addProperties(PARNEW_CLEANUP_DURATION, cleanupDuration);
+        }
+        String heapUsageBefore;
+        if(StringUtils.isNotEmpty(heapUsageBefore = matcher.group(HEAP_USAGE_BEFORE))){
+            data.addProperties(HEAP_USAGE_BEFORE, heapUsageBefore);
+        }
+        String heapUsageAfter;
+        if(StringUtils.isNotEmpty(heapUsageAfter = matcher.group(HEAP_USAGE_AFTER))){
+            data.addProperties(HEAP_USAGE_AFTER, heapUsageAfter);
+        }
+        String heapSize;
+        if(StringUtils.isNotEmpty(heapSize = matcher.group(HEAP_SIZE))){
+            data.addProperties(HEAP_SIZE, heapSize);
+        }
+        String duration;
+        if(StringUtils.isNotEmpty(duration = matcher.group(PARNEW_DURATION))){
+            data.addProperties(PARNEW_DURATION, duration);
+        }
+        //
+        data.setType(GCType.PARNEW);
     }
 }
